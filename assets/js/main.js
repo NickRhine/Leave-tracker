94cf0edd1804e0c00ceb72169c3af366315bfb7b
Nick <nicholas.hobden@rhinemechatronics.com>
2025-04-01 15:20:57 +0200

    Read from  excel file on sharepoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ import { canUse } from "./exports.js";
       clientId: "cf1a04b4-d42a-4e18-9ec3-18ec43bfeaf9",
       authority:
         "https://login.microsoftonline.com/14e5adcf-4446-4cee-bac9-e293492fa769",
-      redirectUri: "http://localhost:5500/html/mainpage.html",
+      redirectUri: "http://localhost:5500/index.html",
     },
     cache: {
       cacheLocation: "localStorage", // This configures where your cache will be stored
@@ -140,31 +140,37 @@ import { canUse } from "./exports.js";
       // Hide message.
       $message._hide();
 
-      var email = document.querySelector("#email").value.trim();
-      var password = document.querySelector("#password").value.trim();
+      // var email = document.querySelector("#email").value.trim();
+      // var password = document.querySelector("#password").value.trim();
 
-      document.querySelector("#email").classList.remove("input-error");
-      document.querySelector("#password").classList.remove("input-error");
+      // document.querySelector("#email").classList.remove("input-error");
+      // document.querySelector("#password").classList.remove("input-error");
 
-      if (password === "") {
-        document.querySelector("#password").classList.add("input-error");
-        $message._show("failure", "Password cannot be empty");
-        $submit.disabled = false;
-        return;
-      }
+      // if (password === "") {
+      //   document.querySelector("#password").classList.add("input-error");
+      //   $message._show("failure", "Password cannot be empty");
+      //   $submit.disabled = false;
+      //   return;
+      // }
 
-      if (email === "" || !email.includes("@")) {
-        document.querySelector("#email").classList.add("input-error");
-        $message._show("failure", "Please enter a valid email");
-        $submit.disabled = false;
-        return;
-      }
+      // if (email === "" || !email.includes("@")) {
+      //   document.querySelector("#email").classList.add("input-error");
+      //   $message._show("failure", "Please enter a valid email");
+      //   $submit.disabled = false;
+      //   return;
+      // }
       // Disable submit.
       $submit.disabled = true;
 
       // Microsoft login
       const loginRequest = {
-        scopes: ["User.Read", "Mail.Read"], // Define the necessary Microsoft Graph API permissions
+        scopes: [
+          "User.Read",
+          "Mail.Read",
+          "Sites.Read.All",
+          "Files.Read.All",
+          "Sites.ReadWrite.All",
+        ], // Define the necessary Microsoft Graph API permissions
       };
 
       msalInstance
@@ -173,7 +179,38 @@ import { canUse } from "./exports.js";
           console.log("Login Successful", response);
 
           const accessToken = response.accessToken;
-          getUserProfile(accessToken);
+          msalInstance.setActiveAccount(response.account); // Set the active account
+
+          fetch("https://graph.microsoft.com/v1.0/me", {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + accessToken,
+            },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("User Profile:", data);
+              // Save user data to sessionStorage
+              sessionStorage.setItem("userProfile", JSON.stringify(data));
+              sessionStorage.setItem("accessToken", accessToken); // Store the access token in sessionStorage
+              sessionStorage.setItem("email", data.mail); // Store the email in sessionStorage
+
+              return msalInstance.acquireTokenSilent({
+                scopes: ["Sites.Read.All", "Files.Read.All"],
+              });
+            })
+            .then((tokenResponse) => {
+              console.log("Sharepoint token acquired", tokenResponse);
+              sessionStorage.setItem(
+                "sharepointToken",
+                tokenResponse.accessToken
+              ); // Store the new access token in sessionStorage
+
+              window.location.href = "http://localhost:5500/html/mainpage.html"; // Redirect to the desired page after successful login
+            })
+            .catch((error) => {
+              console.error("Error during authentication", error);
+            });
 
           $message._show("success", "Login Successful!");
         })
@@ -182,62 +219,6 @@ import { canUse } from "./exports.js";
           $message._show("failure", "Login failed");
           $submit.disabled = false;
         });
-      // Process form.
-      // Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-      // but there's enough here to piece together a working AJAX submission call that does.
-      // setTimeout(function () {
-      //   // Reset form.
-      //   $form.reset();
-
-      //   // Enable submit.
-      //   $submit.disabled = false;
-      //   $message.classList.remove("failure");
-
-      //   // Show message.
-      //   $message._show = function (type, text) {
-      //     $message.innerHTML = text;
-      //     $message.classList.add(type);
-      //     $message.classList.add("visible");
-
-      //     setTimeout(function () {
-      //       $message._hide();
-      //     }, 3000);
-      //   };
-      //   $message._show("success", "Login Successful!");
-      // }, 750);
     });
-
-    function getUserProfile(token) {
-      fetch("https://graph.microsoft.com/v1.0/me", {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("User Profile:", data);
-          // You can now use the user profile data as needed
-          // For example, you can display the user's name or email on the page
-          document.querySelector(
-            "#user-name"
-          ).textContent = `Welcome, ${data.displayName}`;
-        })
-        .catch((error) => {
-          console.error("Error fetching user profile:", error);
-        });
-    }
   })();
 })();
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   let glow = document.createElement("div");
-//   glow.classList.add("cursor-glow");
-//   document.body.appendChild(glow);
-
-//   document.addEventListener("mousemove", function (event) {
-//     let x = event.clientX - 50;
-//     let y = event.clientY - 50;
-//     glow.style.transform = `translate(${x}px, ${y}px)`;
-//   });
-// });
--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -5,6 +5,21 @@ import { SlideShowBG } from "./exports.js";
 
   var $body = document.querySelector("body"); //selects body tag from html
 
+  //Get user data
+  document.addEventListener("DOMContentLoaded", function () {
+    // Get user data from sessionStorage
+    const userProfile = sessionStorage.getItem("userProfile");
+
+    if (userProfile) {
+      const userData = JSON.parse(userProfile);
+      document.querySelector(
+        "#user-name"
+      ).textContent = `Hello, ${userData.displayName}`;
+    } else {
+      console.error("User data not found in sessionStorage.");
+    }
+  });
+
   // Play initial animations on page load.
   window.addEventListener("load", function () {
     window.setTimeout(function () {
@@ -31,4 +46,90 @@ import { SlideShowBG } from "./exports.js";
       upcomingLeave.textContent = "No upcoming leave";
     }
   });
+
+  // Call this function on page load
+  document.addEventListener("DOMContentLoaded", updateLeaveInfo);
 })();
+
+async function getSiteId(accessToken) {
+  const url = `https://graph.microsoft.com/v1.0/sites/netorg7968809.sharepoint.com:/sites/RHINEmechatronics-BusinessDevelopment`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await response.json();
+  if (response.ok) {
+    return data.id;
+  } else {
+    console.error("Error fetching Site ID:", data);
+    return null;
+  }
+}
+
+async function getFileId(accessToken, siteId) {
+  const url = `https://graph.microsoft.com/v1.0/sites/${siteId}/drive/root:/Rename.xlsx`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await response.json();
+  if (response.ok) {
+    return data.id;
+  } else {
+    console.error("Error fetching File ID:", data);
+    return null;
+  }
+}
+
+async function getExcelData(accessToken, siteId, fileId) {
+  const url = `https://graph.microsoft.com/v1.0/sites/${siteId}/drive/items/${fileId}/workbook/worksheets('Data')/range(address='A1:A2')`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await response.json();
+  if (response.ok) {
+    return data.values;
+  } else {
+    console.error("Error fetching Excel Data:", data);
+    return null;
+  }
+}
+
+async function updateLeaveInfo() {
+  const accessToken = sessionStorage.getItem("accessToken");
+
+  if (!accessToken) {
+    console.error("Access Token not found.");
+    return;
+  }
+
+  // const siteId = await getSiteId(accessToken);
+  // if (!siteId) return;
+  const siteId =
+    "netorg7968809.sharepoint.com,d6ef5094-875f-47d7-93c4-43ae171a04ff,883a8121-0374-49f4-9476-2d3b9a1cb38a";
+
+  const fileId = "012LJMUY6BHXDWVGWPI5DIT3YPOFVODUTI";
+  // "b!lFDv1l-H10eTxEOuFxoE_yGBOoh0A_RJlHYtO5ocs4oWLWqGN5CiRLBtt1hFEdjV";
+  // if (!fileId) return;
+
+  const excelData = await getExcelData(accessToken, siteId, fileId);
+  // const excelData = "012LJMUY6BHXDWVGWPI5DIT3YPOFVODUTI";
+  // if (!excelData) return;
+
+  // Assuming column A is "Leave Type" and column B is "Days Remaining"
+  document.querySelector("#leave-balance").textContent = excelData[0][0]; // Adjust index based on your Excel structure
+  document.querySelector("#upcoming-leave").textContent = excelData[1][0]; // Adjust index based on your Excel structure
+}
