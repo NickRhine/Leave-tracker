dd79ecff0bf54d1489ed0533e01048941d5e7a24
Nick <nicholas.hobden@rhinemechatronics.com>
2025-04-07 15:54:25 +0200

    Emailing in progress.

--- assets/js/leave_application.js
+++ assets/js/leave_application.js
@@ -51,4 +51,57 @@ import { SlideShowBG } from "./exports.js";
       "total-days"
     ).textContent = `Total working days requested: ${workingDays}`;
   }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    document
+      .querySelector("leave-submit input")
+      .addEventListener("click", submitForm());
+  });
 })();
+
+// Save form data and open email to send formatted document
+async function submitForm(event) {
+  event.preventDefault(); // Prevent from submission and page reload
+}
+
+//Generate a pdf using a screenshot of the form
+function generatePDF() {
+  const userProfile = sessionStorage.getItem("userProfile");
+  let username = ""; // Default value
+  if (userProfile) {
+    const userData = JSON.parse(userProfile);
+    username = userData.displayName;
+  } else {
+    console.error("User data not found in sessionStorage.");
+  }
+
+  const element = document.body; // The content you want to convert into PDF
+  const opt = {
+    margin: 1,
+    filename: `leave_request_${username}.pdf`,
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  };
+
+  // Generate PDF
+  html2pdf().from(element).set(opt).save();
+}
+
+// Attach pdf to email and open email client
+function sendEmailWithPDF(pdfData) {
+  emailjs
+    .send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
+      user_email: "nicholas.hobden@rhinemechatronics.com", // The email of the recipient
+      subject: "Leave Request",
+      message: "Please find attached the leave request PDF.",
+      attachment: pdfData, // Send the PDF as an attachment
+    })
+    .then((response) => {
+      console.log("Email sent successfully:", response);
+      alert("Email sent successfully!");
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+    });
+}
